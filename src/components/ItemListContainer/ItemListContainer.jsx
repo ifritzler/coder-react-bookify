35a3835b245cddbefa5ae2f09671e5c2434d8f6d
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-08-24 17:59:24 -0300

    restore part of the missing ItemCount

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,15 @@
+// import { ItemCounter } from '../ItemCounter'
 import styles from './style.module.css'
 
 const ItemListContainer = ({ greeting }) => {
 	return (
 		<div className={styles['item-list']}>
 			<p>{`Hola ${greeting || 'Usuario'}!`}</p>
+			<div className={styles.items}>
+				{/* <ItemCounter productName={'Book 1'} />
+				<ItemCounter productName={'Book 2'} />
+				<ItemCounter productName={'Book 3'} /> */}
+			</div>
 		</div>
 	)
 }
