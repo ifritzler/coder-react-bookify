28f567f686e0918bcc9c8426e251281fd843933c
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-08-24 04:21:14 -0300

    navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,46 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons'
 import styles from './style.module.css'
+import { useState } from 'react'
 
 const Navbar = () => {
+	const [isActive, setIsActive] = useState(false)
+
 	return (
 		<nav className={styles.nav}>
 			<ul>
 				<li className={styles.logo}>Bookify</li>
-				<div className={styles.links}>
-					<li>
-						<a href='#'>Explorar</a>
-					</li>
-					<li>
-						<a href='#'>Best Sellers</a>
-					</li>
-					<li>
-						<a href='#'>Novelas</a>
-					</li>
-					<li>
-						<a href='#'>Manga</a>
-					</li>
-				</div>
 				<li className={styles['search-icon']}>
 					<input type='search' placeholder='Search' />
 					<div className={styles.icon}>
 						<FontAwesomeIcon icon={faSearch} />
 					</div>
 				</li>
+				<div className={styles['menu-bars']} onClick={() => setIsActive(!isActive)}>
+					<FontAwesomeIcon icon={faBars} />
+				</div>
+				<div className={`${styles.links} ${isActive ? styles['menu--open'] : ''}`}>
+					<li>
+						<a href='#' onClick={() => setIsActive(false)}>
+							Explorar
+						</a>
+					</li>
+					<li>
+						<a href='#' onClick={() => setIsActive(false)}>
+							Best Sellers
+						</a>
+					</li>
+					<li>
+						<a href='#' onClick={() => setIsActive(false)}>
+							Novelas
+						</a>
+					</li>
+					<li>
+						<a href='#' onClick={() => setIsActive(false)}>
+							Manga
+						</a>
+					</li>
+				</div>
 			</ul>
 		</nav>
 	)
