e8d146f2c7da30056603b9f0b6920d68a2640503
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-08-30 17:01:09 -0300

    set loading text in fetch

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,24 +3,34 @@ import { Item } from '../Item'
 import styles from './style.module.css'
 
 const ItemListContainer = () => {
-  //	TODO Logica para hacer fetch a los productos
   // TODO Este componente debe tener un titulo que indique la o las categorias que este mostrando.
   const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getData = async () => {
       try {
+        setLoading(true)
         const response = await fetch('src/data/books.json')
         const data = await response.json()
-        setItems(data)
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(data)
+          }, 2000)
+        })
       } catch {}
     }
-    getData()
+    getData().then((data) => {
+      setLoading(false)
+      setItems(data)
+    })
   }, [])
 
   return (
     <section className={styles['item-list']}>
       <div className={styles.container}>
+        {/* Renderizado condicional de un loading */}
+        {loading && <p className={styles.loading}>Cargando</p>}
         <div className={styles.items}>
           {items.map((item) => {
             return <Item key={item.id} data={item} />
