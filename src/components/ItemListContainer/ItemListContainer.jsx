028af22eaddbd0cf0ca7a387c20789923a597729
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-08-30 16:25:07 -0300

    remove styles from ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,23 +1,40 @@
-import { ItemCount } from '../ItemCount'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { Item } from '../Item'
 import styles from './style.module.css'
 
 const ItemListContainer = () => {
   //	TODO Logica para hacer fetch a los productos
   // TODO Este componente debe tener un titulo que indique la o las categorias que este mostrando.
+  const [items, setItems] = useState([])
+
   const onAdd = (count) => {
     alert(`Se agregaron ${count} productos al carrito`)
   }
   const addFail = () => {
     alert('Fallo la carga del producto en el carrito')
   }
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch('src/data/books.json')
+        const data = await response.json()
+        setItems(data)
+      } catch {}
+    }
+    getData()
+  }, [])
   return (
-    <div className={styles['item-list']}>
-      <div className={styles.items}>
-        <ItemCount stock={5} initial={1} onAdd={onAdd} addFail={addFail} />
-        <ItemCount stock={15} initial={1} onAdd={onAdd} addFail={addFail} />
-        <ItemCount stock={3} initial={1} onAdd={onAdd} addFail={addFail} />
+    <section className={styles['item-list']}>
+      <div className={styles.container}>
+        <div className={styles.items}>
+          {items.map((item) => {
+            return <Item key={item.id} data={item} />
+          })}
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
