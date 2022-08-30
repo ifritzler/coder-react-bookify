4cc5ea6307daa227aac2d2f86722eab67bfae0c9
Ilan Emanuel Fritzler <contacto.fritzlerilan@gmail.com>
2022-08-30 15:28:22 -0300

    make a mobile view for the navbar

--- src/components/NewNavbar/NewNavbar.jsx
+++ src/components/NewNavbar/NewNavbar.jsx
@@ -1,6 +1,84 @@
 import styles from './styles.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 const NewNavbar = () => {
-	return <div>NewNavbar</div>
+	const [isActive, setIsActive] = useState(false)
+
+	return (
+		<div className={styles.navbar}>
+			<div className={styles.container}>
+				{/* Logo */}
+				<span className={styles.logo}>Bookify</span>
+
+				{/* Toggle menu icon */}
+				<span className={styles.menu} onClick={() => setIsActive(!isActive)}>
+					<FontAwesomeIcon icon={faBars} />
+				</span>
+
+				{/* Links */}
+				<nav>
+					<ul className={`${styles.links} ${isActive ? styles.active : ''}`}>
+						<li>
+							<a
+								href='#'
+								className={styles.link}
+								onClick={() => {
+									setIsActive(false)
+								}}
+							>
+								BestSellers
+							</a>
+						</li>
+						<li>
+							<a
+								href='#'
+								className={styles.link}
+								onClick={() => {
+									setIsActive(false)
+								}}
+							>
+								Novelas
+							</a>
+						</li>
+						<li>
+							<a
+								href='#'
+								className={styles.link}
+								onClick={() => {
+									setIsActive(false)
+								}}
+							>
+								Fantasia
+							</a>
+						</li>
+						<li>
+							<a
+								href='#'
+								className={styles.link}
+								onClick={() => {
+									setIsActive(false)
+								}}
+							>
+								Ciencia ficcion
+							</a>
+						</li>
+						<li>
+							<a
+								href='#'
+								className={styles.link}
+								onClick={() => {
+									setIsActive(false)
+								}}
+							>
+								Manga
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	)
 }
 export default NewNavbar
